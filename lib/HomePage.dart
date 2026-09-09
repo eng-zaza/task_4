480883db7f16ce17588c1637bcfe5b1b4bc02aeb
Karim <karimahmedrageb27.6.2006@gmail.com>
2026-09-09 21:58:32 +0300

    Task 4

---
+++ lib/HomePage.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      // Floating Action Button
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter=0;
+          });
+        },
+        child: const Icon(Icons.refresh ,color: Colors.redAccent,),
+      ),
+
+      // AppBar
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        centerTitle: true,
+
+        // Leading Icon
+        leading: const Icon(
+          Icons.home,color: Colors.lightBlueAccent,
+        ),
+
+        // Action Icon
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                counter--;
+              });
+            },
+            icon: const Icon(
+              Icons.remove,color: Colors.red,
+            ),
+          ),
+        ],
+      ),
+
+      // Body
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            
+            Container(
+              width: 300,
+              height: 100,
+              color: Colors.green,
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Increment',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        counter++;
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            
+            Container(
+              width: 300,
+              height: 100,
+              color: Colors.blue,
+              alignment: Alignment.center,
+              child: Text(
+                'Counter: $counter',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            // Container 3 - Decrement
+            Container(
+              width: 300,
+              height: 100,
+              color: Colors.red,
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Decrement',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        counter--;
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.remove,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
